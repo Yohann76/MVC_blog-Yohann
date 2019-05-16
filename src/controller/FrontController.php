5136a49f506d98cee563843e1e1d81e3aaef941d
Yohann76 <yohanndurand76@gmail.com>
2019-05-16 22:36:15 +0200

    add download cv

--- src/controller/FrontController.php
+++ src/controller/FrontController.php
@@ -35,18 +35,6 @@ class FrontController extends Controller
         return $this->view->render('connection', [
         ]);
     }
-    // Download CV
-    public function downloadCv()
-    {
-        $file_name ="../public/cv.pdf";
-        header("Content-Disposition: attachment; filename=\"$file_name\"");
-
-
-    }
-
-
-
-
 
     public function Déconnexion()
     {
@@ -54,5 +42,30 @@ class FrontController extends Controller
         header('Location: ../public/index.php?route=connection');
     }
 
+    // Download CV
+    public function downloadCv()
+    {
+        $full_path = '../public/cv.pdf';
+        $file_name = basename($full_path);
+
+        ini_set('zlib.output_compression', 0);
+        $date = gmdate(DATE_RFC1123);
+
+        header('Pragma: public');
+        header('Cache-Control: must-revalidate, pre-check=0, post-check=0, max-age=0');
+
+        header('Content-Tranfer-Encoding: none');
+        header('Content-Length: '.filesize($full_path));
+        header('Content-MD5: '.base64_encode(md5_file($full_path)));
+        header('Content-Type: application/octetstream; name="'.$file_name.'"');
+        header('Content-Disposition: attachment; filename="'.$file_name.'"');
+
+        header('Date: '.$date);
+        header('Expires: '.gmdate(DATE_RFC1123, time()+1));
+        header('Last-Modified: '.gmdate(DATE_RFC1123, filemtime($full_path)));
+
+        readfile($full_path);
+        exit;
+    }
 
 }
